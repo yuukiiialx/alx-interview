64ea72fab3712ed4bdb288b1294a818dda0b6b76
Mohamed <alxmohamedemadeldin@gmail.com>
2024-06-25 16:06:49 +0000

    Solved Task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""pascal_triangle module"""
+
+
+def pascal_triangle(n):
+    """pascal_triangle
+    arg:
+        n: int
+    return list of int list of pascal triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for row_num in range(1, n):
+        row = [1] * (row_num + 1)
+        triangle.append(row)
+        for i in range(1, len(row) - 1):
+            row[i] = triangle[row_num - 1][i - 1] + triangle[row_num - 1][i]
+
+    return triangle
