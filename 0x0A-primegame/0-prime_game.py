df542bf804d63ba629e0f35861bf075b2aa6e6dc
Mohamed <alxmohamedemadeldin@gmail.com>
2024-09-05 12:38:57 +0000

    Solved

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,46 @@
-#!/usr/bin/python3
-"""Module defining isWinner function."""
-
-
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
-
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
-
-        if not primesSet:
-            benWinsCount += 1
-            continue
-
-        isMariaTurns = True
-
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+    def compute_grundy_numbers(max_n):
+        grundies = [0] * (max_n + 1)
+        primes = []
+        
+        is_prime = [True] * (max_n + 1)
+        is_prime[0] = is_prime[1] = False
+        
+        for i in range(2, max_n + 1):
+            if is_prime[i]:
+                primes.append(i)
+                for multiple in range(i * i, max_n + 1, i):
+                    is_prime[multiple] = False
+        
+        for n in range(2, max_n + 1):
+            reachable = set()
+            for prime in primes:
+                if prime > n:
+                    break
+                for multiple in range(prime, n + 1, prime):
+                    reachable.add(grundies[n - multiple])
+            mex = 0
+            while mex in reachable:
+                mex += 1
+            grundies[n] = mex
+        
+        return grundies
+    
+    max_n = max(nums)
+    grundies = compute_grundy_numbers(max_n)
+    
+    maria_wins = 0
+    ben_wins = 0
+    
+    for n in nums:
+        if grundies[n] != 0:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+    
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
